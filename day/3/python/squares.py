80c485955862382fefcf23c99cde04a9ca61df8a
Simon Fraser <simonfr@gmail.com>
2017-12-03 17:21:28 +0000

    Python day 3, stage 2

--- day/3/python/squares.py
+++ day/3/python/squares.py
@@ -2,6 +2,8 @@
 from math import ceil, sqrt
 from operator import add
 import timeit
+from enum import Enum
+
 
 """
 65  64  63  62  61  60  59  58  57
@@ -58,17 +60,18 @@ def faster_coords(dest_index):
     square_start = pow((size - 2), 2) + 1
     ss_coords = (size - layer), (layer - size + 1)
     final = adjust(size, ss_coords, square_start, dest_index)
+
     return final
 
 
-def brute_force(dest_index):
+def brute_force(start_coords, dest_index):
 
     if dest_index == 1:
         return 0, 0
 
     size = 2  # travel size, rather than square dimension
     # Because of our earlier checks, we'll always start at 2
-    x, y = 0, 0
+    x, y = start_coords
     distance = dest_index - 1
     while distance:
         # bump to next square_layer
@@ -109,24 +112,79 @@ def carry_data(dest_index):
         cheat_coords = faster_coords(dest_index)
         c_md = manhattan_distance((0, 0), cheat_coords)
 
-    brute_coords = brute_force(dest_index)
+    brute_coords = brute_force((0, 0), dest_index)
     b_md = manhattan_distance((0, 0), brute_coords)
     # print("Brute force: {}, Cheat: {}".format(brute_coords, cheat_coords))
 
     print("{} is {} away, or possibly {}".format(dest_index, b_md, c_md))
 
-    def wrap_cheat():
-        faster_coords(dest_index)
 
-    cheat_timing = timeit.timeit(wrap_cheat)
-    print("Shortcut elapsed time: {}".format(cheat_timing))
+class Direction(Enum):
+    up = 1
+    right = 2
+    down = 3
+    left = 4
 
-    def wrap_brute():
-        brute_force(dest_index)
 
-    brute_timing = timeit.timeit(wrap_brute)
-    print("Brute force elapsed time: {}".format(brute_timing))
-    print("")
+class Point(object):
+    """docstring for Point."""
+
+    def __init__(self, index, x, y, value=None):
+        self.index = index
+        self.x = x
+        self.y = y
+        self.value = value
+
+    def __repr__(self):
+        return "Point {}: {},{}: {}".format(self.index, self.x, self.y, self.value)
+
+
+def adjacent(p1, p2):
+    if abs(p1.x - p2.x) > 1:
+        return False
+    if abs(p1.y - p2.y) > 1:
+        return False
+    return True
+
+
+def stress_test(threshold):
+
+    storage = [Point(1, 0, 0, 1)]
+
+    x, y = 0, 0
+    size = 1
+    location = 1
+    direction = Direction.right
+
+    while storage[-1].value <= threshold:
+        if direction == Direction.right:
+            x += 1
+            if x == size and y == -size:
+                size += 1
+            if x == size:
+                direction = Direction.up
+        elif direction == Direction.up:
+            y += 1
+            if y == size:
+                direction = Direction.left
+        elif direction == Direction.left:
+            x -= 1
+            if x == -size:
+                direction = Direction.down
+        elif direction == Direction.down:
+            y -= 1
+            if y == -size:
+                direction = Direction.right
+        location += 1
+
+        newpoint = Point(location, x, y)
+        value = 0
+        for point in storage:
+            if adjacent(point, newpoint):
+                value += point.value
+        newpoint.value = value
+        storage.append(newpoint)
+    print("Stress test found: {}".format(storage[-1].value))
 
 
 if __name__ == '__main__':
@@ -136,3 +194,4 @@ if __name__ == '__main__':
     carry_data(82)
     carry_data(1024)
     carry_data(312051)
+    stress_test(312051)
